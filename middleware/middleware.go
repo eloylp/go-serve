f42c82e246726263ca32e55625aeef92065f70ec
eloylp <eloylpdev@gmail.com>
2020-02-25 11:41:33 +0100

    Improve docs for package middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,12 +4,13 @@ import (
 	"net/http"
 )
 
-// Middleware accepts an http.Handler as parameter
-// and returns the next
+// Middleware accepts the next http.Handler as parameter
+// and returns current one that may modify request/writer
+// and finally calls the handler passed as parameter.
 type Middleware func(h http.Handler) http.Handler
 
 // Apply will take the handler as first parameter.
-// The variadic function accepts any number of middlewares
+// The variadic part of function accepts any number of middlewares
 // that will be called in the passed order.
 // Beware that the handler will always be called as the
 // last element of the chain.
