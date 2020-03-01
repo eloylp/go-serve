bdac6e738d401a03b001331bdf5fcdb3330198d7
eloylp <eloylpdev@gmail.com>
2020-03-01 17:23:24 +0100

    Implement the config package

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/eloylp/go-serve/config"
 	"github.com/eloylp/go-serve/handler"
 	"github.com/eloylp/go-serve/logging"
 	"github.com/eloylp/go-serve/www"
@@ -15,25 +15,17 @@ var version string
 
 func main() {
 
-	currentDir, err := os.Getwd()
+	logger := logging.NewConsoleLogger()
+
+	docRoot, prefix, listenAddr, err := config.FromArgs(os.Args)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	var docRoot string
-	var prefix string
-	var listenAddr string
-
-	flag.StringVar(&docRoot, "d", currentDir, "Defines the document root")
-	flag.StringVar(&prefix, "p", "/", "Defines prefix to use for serve files")
-	flag.StringVar(&listenAddr, "l", "0.0.0.0:8080", "Defines the listen address")
-	flag.Parse()
-
 	fmt.Println(fmt.Sprintf("go-serve %s", version))
 	log.Println(fmt.Sprintf("Starting to serve %s at %s ...", docRoot, listenAddr))
 
 	fileHandler := http.FileServer(http.Dir(docRoot))
-	logger := logging.NewConsoleLogger()
 	http.Handle(prefix, http.StripPrefix(prefix, www.Apply(fileHandler, handler.ServerHeader(version), handler.RequestLogger(logger))))
 	if err := http.ListenAndServe(listenAddr, nil); err != http.ErrServerClosed {
 		log.Fatal(err)
