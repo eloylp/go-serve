624bb99045536b63eb5c3f6082105bd4ff620cc0
eloylp <eloylpdev@gmail.com>
2021-03-31 22:30:06 +0200

    Add Copy() test helper

--- server/server_helper_test.go
+++ server/server_helper_test.go
@@ -3,8 +3,12 @@ package server_test
 import (
 	"crypto/md5"
 	"fmt"
+	"io"
+	"io/fs"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -24,4 +28,44 @@ func BodyFrom(t *testing.T, url string) []byte {
 func md5From(data []byte) string {
 	md5.New()
 	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+// Copy will copy directories and files recursively from one root to another.
+// This function will abort the test if any operation files. This function will
+// not do any type on clean up on fail. This is recommended to use in conjunction
+// to testing.TempDir() .
+func Copy(t *testing.T, source, dest string) {
+	err := filepath.Walk(source, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		sourceRel, err := filepath.Rel(source, path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			err = os.MkdirAll(filepath.Join(dest, sourceRel), 0777)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		fileFrom, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer fileFrom.Close()
+		fileTo, err := os.Create(filepath.Join(dest, sourceRel))
+		if err != nil {
+			return err
+		}
+		defer fileTo.Close()
+		if _, err := io.Copy(fileTo, fileFrom); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
 }
