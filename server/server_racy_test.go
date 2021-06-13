9eb5c97b87e77f5d008bdfe1b619d787dc6d1848
eloylp <eloylpdev@gmail.com>
2021-06-13 15:58:28 +0200

    Completing racy test

--- server/server_racy_test.go
+++ server/server_racy_test.go
@@ -10,12 +10,17 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
-	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"go.eloylp.dev/kit/exec"
 	"go.eloylp.dev/kit/test"
 
 	"go.eloylp.dev/go-serve/config"
+	"go.eloylp.dev/go-serve/server"
 )
 
 var (
@@ -39,34 +44,72 @@ var (
 			Transport: tr,
 		}
 	}()
+	uploadCount   int32
+	downloadCount int32
 )
 
 func TestServerExecutionPaths(t *testing.T) {
 	BeforeEach(t)
-	server, _, docRoot := sut(t,
+	logger := bytes.NewBuffer(nil)
+	s, _, docRoot := sut(t,
 		config.WithReadAuthorizations(testUserCredentials),
 		config.WithWriteAuthorizations(testUserCredentials),
+		config.WithLoggerLevel(logrus.DebugLevel.String()),
+		config.WithLoggerOutput(logger),
 	)
-	defer server.Shutdown(context.Background())
+	defer s.Shutdown(context.Background())
+	ctx, cancl := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancl()
 	test.Copy(t, DocRoot, docRoot)
-
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		<-ctx.Done()
+		wg.Done()
+	}()
+	go exec.Parallelize(ctx, wg, 1, uploadFile)
+	go exec.Parallelize(ctx, wg, 1, downloadFile)
+	wg.Wait()
+	fmt.Printf("Racy test stats >> uploaded %v . downloaded %v", uploadCount, downloadCount) //nolint:forbidigo
+	fmt.Println("--- LOG Trace ---")
+	io.Copy(os.Stdout, logger)
 }
 
 func uploadFile() {
-	fileName := fmt.Sprintf("%s-tux.png", uuid.NewString())
-	req, err := http.NewRequest(http.MethodGet, HTTPAddressUpload, bytes.NewReader(tuxContent))
+	fileName := fmt.Sprintf("%v-tux.png", atomic.LoadInt32(&uploadCount))
+	req, err := http.NewRequest(http.MethodPost, HTTPAddressUpload, bytes.NewReader(tuxContent))
 	if err != nil {
 		panic(err)
 	}
+	req.SetBasicAuth("user", "password")
 	req.Header.Add(DeployPathHeader, "/"+fileName)
+	req.Header.Add("Content-Type", server.ContentTypeFile)
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
-	_, _ = io.Copy(ioutil.Discard, resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("expected 200 got %v", resp.StatusCode))
+	}
+	_, _ = io.Copy(io.Discard, resp.Body)
 	_ = resp.Body.Close()
+	atomic.AddInt32(&uploadCount, 1)
 }
 
 func downloadFile() {
-
+	req, err := http.NewRequest(http.MethodGet, HTTPAddressStatic+"/gnu.png", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.SetBasicAuth("user", "password")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	_, _ = io.Copy(io.Discard, resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("expected 200 got %v", resp.StatusCode))
+	}
+	atomic.AddInt32(&downloadCount, 1)
 }
